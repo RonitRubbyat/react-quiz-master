41439aa76cf1be5bff22d3927de7c241b0d85584
RonitRubbyat <ronith127242016@gmail.com>
2023-03-19 01:24:36 +0600

    home data loded

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const quizType = useLoaderData();
+    const { data } = quizType;
+    console.log(data);
     return (
         <div>
             <h2>Home</h2>
